0e712a527790e7e24f4139a6d71e642032d09631
Rivaaj Jumna <50959956+rivms@users.noreply.github.com>
2021-03-17 00:39:53 +0800

    Initial commit

---
+++ daxcli/Dax.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AnalysisServices.AdomdClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace daxcli
+{
+    public class Dax
+    {
+        private string connectionString;
+        public Dax(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public DataTable Query(string query)
+        {
+            var dt = new DataTable();
+
+            using (var conn = new AdomdConnection(connectionString))
+            {
+                conn.Open();
+
+                var da = new AdomdDataAdapter(query, conn);
+                da.Fill(dt);
+            }
+
+            return dt;
+
+        }
+
+        public void Display(DataTable dt)
+        {
+            foreach (DataColumn col in dt.Columns)
+            {
+                Console.Write(col.ColumnName + ", ");
+            }
+
+            Console.WriteLine();
+
+            foreach(DataRow dr in dt.Rows)
+            {
+                foreach(var cell in dr.ItemArray)
+                {
+                    Console.Write(cell + ", ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ daxcli/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+
+namespace daxcli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connString = "Data Source=.;Catalog=WorldWideImportersTAB;";
+
+            var dax = new Dax(connString);
+
+            var query = 
+@"DEFINE 
+VAR SelectedColors =
+FILTER(
+	ALL( 'Item'[Color] ),
+	'Item'[Color] IN { ""Red"", ""Green"" }
+)
+
+EVALUATE
+CALCULATETABLE(
+    'Item',
+    SelectedColors
+)";
+
+            var dt = dax.Query(query);
+
+            dax.Display(dt);
+            Console.WriteLine($"Rows returned: {dt.Rows.Count}");
+        }
+    }
+}
